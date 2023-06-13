1e8f8715a06ed81cf3089a4c8cc045bb0d72ba6a
Anindo <anindo@bridgelabz.com>
2023-06-13 16:19:07 +0530

    Modified to use JSX

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <h1>Hello World !!! </h1>
       <h2> Learning Ract JS</h2>
     </div>
   );
+}*/
+
+function App(){
+  const jsx = <h1> JSX is Awesome</h1>;
+  return jsx;
 }
 
 export default App;
