ae6d0a090487cab7f43c63bfc98cd21bb1545a68
Anindo <anindo@bridgelabz.com>
2023-06-14 06:52:06 +0530

    Modified to use variables, condition and iterating over an array using map in JSX

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -10,8 +10,30 @@ import './App.css';
 }*/
 
 function App(){
-  const jsx = <h1> JSX is Awesome</h1>;
-  return jsx;
-}
+  const topic = "React";
+  const concepts = ["JSX", "Props", "State", "Hook"];
+  // Add array index as the key
+  /*const renderList = concepts.map((item, index) => 
+    <li key={index}>{item}</li>
+    );*/
+  return (
+    <div className="App">
+      {/* JSX code */}
+      <h1 id = "header-id"> {topic} is Awesome </h1>
+      {/*{renderList}*/}
+      <h5 >{String(true)} ?</h5>
 
+      {
+        true ? <h4> yes it is true</h4> : null
+      }
+
+      <h2> Concepts to learn are : </h2>
+
+      {
+        concepts.map(concept => <h4 key = {concept}><li>{concept}</li></h4>)
+      }
+    </div>
+  )
+
+}
 export default App;
