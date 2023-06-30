5dbe1a2e2fdf0d38bba2153fd3bfb3b0243125d2
Anindo <anindo@bridgelabz.com>
2023-06-30 11:08:01 +0530

    Modified the Basic App to list few desserts items with some random images

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,25 +2,52 @@ import './App.css';
 
 
 function App(){
-  const topic = "React";
-  const concepts = ["JSX", "Props", "State", "Hook"];
+  //const topic = "React";
+  //const concepts = ["JSX", "Props", "State", "Hook"];
+  const menuItems = [
+	{
+		"title" : "Tiramisu",
+    "type" : "Desserts",
+		"description" : "Best Tiramisu in Blr",
+		"image" : "https://picsum.photos/200/205/?random",
+		"price" : "Rs 195",
+	},
+	{
+		"title" : "Litchi Ice Cream",
+    "type" : "Desserts",
+		"description" : "Cool Muzaffarpur in hot summer",
+		"image" : "https://picsum.photos/200/210/?random",
+		"price" : "Rs 175",
+	},
+	{
+		"title" : "Death By Chocolate (DBC)",
+    "type" : "Desserts",
+		"description" : "Enjoy the chocolates",
+		"image" : "https://picsum.photos/200/200/?random",
+		"price" : "Rs 225",
+	},
+
+]
+
   
   return (
     <div className="App">
       {/* JSX code */}
-      <h1 id = "header-id"> {topic} is Awesome </h1>
-    
-      <h5 >{String(true)} ?</h5>
+      <h1 id = "header-id"> Desserts Menu</h1>
+      {/*{renderList}*/}
+      <ul>
+        {
+          menuItems.map((menuItems,index) => (
+            <li key = {index}>
+              <h2>{menuItems.title}</h2>
+              <p>{menuItems.description}</p>
+              <img src={menuItems.image} alt={menuItems.title}></img>
+              <h4>{menuItems.price}</h4>
+            </li>
+          ))
+        }
+      </ul>
 
-      {
-        true ? <h4> yes it is true</h4> : null
-      }
-
-      <h2> Concepts to learn are : </h2>
-
-      {
-        concepts.map(concept => <h4 key = {concept}><li>{concept}</li></h4>)
-      }
     </div>
   )
 
