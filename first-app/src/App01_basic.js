6d09c55a74a0ababd377a11cb3c2070e3a20ccd3
Anindo <anindo@bridgelabz.com>
2023-06-30 11:07:02 +0530

    Basic App is copied to App01_basic.js

---
+++ first-app/src/App01_basic.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+
+function App(){
+  const topic = "React";
+  const concepts = ["JSX", "Props", "State", "Hook"];
+  
+  return (
+    <div className="App">
+      {/* JSX code */}
+      <h1 id = "header-id"> {topic} is Awesome </h1>
+      {/*{renderList}*/}
+      <h5 >{String(true)} ?</h5>
+
+      {
+        true ? <h4> yes it is true</h4> : null
+      }
+
+      <h2> Concepts to learn are : </h2>
+
+      {
+        concepts.map(concept => <h4 key = {concept}><li>{concept}</li></h4>)
+      }
+    </div>
+  )
+
+}
+export default App;
