9e950fe49d7cbebbf334f2ec00ef2e2a1952900b
Anindo <anindo@bridgelabz.com>
2023-07-01 11:54:22 +0530

    created base component in MovieComponent.js n imported and used in App.js

--- movie_list/src/App.js
+++ movie_list/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import MovieComponent from './MovieComponent'
 
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1> Learning Component</h1>
+      <MovieComponent />
     
     </div>
   );
