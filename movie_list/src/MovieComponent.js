418c98cf27712012c262d47056b8bbcc4f291fdc
Anindo <anindo@bridgelabz.com>
2023-07-01 13:01:28 +0530

    Modified-MovieComponent.js to add other info on movies

--- movie_list/src/MovieComponent.js
+++ movie_list/src/MovieComponent.js
@@ -81,6 +81,12 @@ const movies = [
                     movies.map((movie)=>(
                         <li className ="movie" key = {movie.id}>
                             <img src ={movie.poster}  alt = {movie.title} />
+                            <p>
+                                {movie.title} by {movie.director} 
+                                was released on {movie.year}
+                            </p>
+                            <p>Rating: {movie.rating}</p>
+
                         </li>
                     )
                     )
