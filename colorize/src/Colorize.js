be2bb4a3e47f6dfd89008b9d8c7ecb11b0d5a0d0
Anindo <anindo@bridgelabz.com>
2023-07-03 17:34:16 +0530

    Modified colourize.js to have random color change

--- colorize/src/Colorize.js
+++ colorize/src/Colorize.js
@@ -3,15 +3,23 @@ import { useState } from "react";
 const Colorizer = ()=> {
     const [color, setColor] =useState('#6d0f0f');
 
+    const getRandomColor = () => {
+        const random = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        return random;
+      }
+
     const changeColor = () => {
-        console.log('Color Change');
-        setColor('green');
+        const randomColor = getRandomColor();
+        console.log('Color Change with', randomColor);
+        setColor(randomColor);
     }
 
     return(
         <div className ='colorizer'>
             <div className ='box' 
-                  style={{backgroundColor:color}}> </div>
+                  style={{backgroundColor:color}}> 
+                {color}
+            </div>
             <button onClick={changeColor}>Change Color</button>
         </div>
     );
