8bac8f238b41be636834ccaf1d974d1cbcd152aa
Anindo <anindo@bridgelabz.com>
2023-07-03 17:24:16 +0530

    Modified App.css - border-radius valuer

--- colorize/src/Colorize.js
+++ colorize/src/Colorize.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Colorizer = ()=> {
+    const [color, setColor] =useState('#6d0f0f');
+
+    const changeColor = () => {
+        console.log('Color Change');
+        setColor('green');
+    }
+
     return(
         <div className ='colorizer'>
-            <div className ='box'></div>
-            <button>Change Color</button>
+            <div className ='box' 
+                  style={{backgroundColor:color}}> </div>
+            <button onClick={changeColor}>Change Color</button>
         </div>
     );
 }
